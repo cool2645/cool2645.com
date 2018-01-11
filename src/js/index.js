23c1b481e1197a7c5575c01e5193a4b6d0dd56c4
LI JIAHAO <lijiahao99131@gmail.com>
2018-01-12 00:11:21 +0800

    parallax

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import "../scss/index.scss"
+
+window.onscroll = function(){
+    let speed = 10;
+    document.getElementById("p1BlackBoard").style.top = (100-window.pageYOffset/speed) + "%";
+    console.log(document.getElementById("p1BlackBoard").style.top )
+}

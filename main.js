ff8edf7eeb8556e42376a6940bd14aa82e89c61a
Riko Sakurauchi <i@riko.love>
2021-10-25 20:04:05 -0400

    :sparkles: add 3d transform

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ export function sendLike (e) {
 }
 
 document.querySelector('#like').addEventListener('click', sendLike)
+document.addEventListener('mousemove', (e) => {
+  const maxVibration = 10
+  const maxRotation = 90
+  const rateX = maxVibration / document.documentElement.offsetWidth
+  const rateY = maxVibration / document.documentElement.offsetHeight
+  const diagonal = Math.sqrt(document.documentElement.offsetWidth ** 2 +
+    document.documentElement.offsetHeight ** 2)
+  const rateD = maxRotation / diagonal
+  const baseX = document.documentElement.offsetWidth / 2
+  const baseY = document.documentElement.offsetHeight / 2
+  const dX = (e.screenX - baseX) / baseX * baseY, dY = (e.screenY - baseY)
+  const d = Math.sqrt(dX ** 2 + dY ** 2)
+  const nX = dX / d, nY = dY / d
+  const tX = dX * rateX, tY = dY * rateY, tD = d * rateD
+  const title = document.querySelector('.title')
+  title.style.transform = `translate(${tX}px, ${tY}px)`
+  const banner = document.querySelector('.banner')
+  banner.style.transform = `rotate3d(${-nY}, ${nX}, 0, ${-tD}deg)`
+})
